c18e139025eb64c094534688bd65ce42ef7da774
Aprajita Bajpai <aprajitabajpai@Aprajitas-MacBook-Pro.local>
2020-09-22 22:37:01 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	//thid is a variable
+	var a = "intial variable"
+	fmt.Println(a)
+
+	//short hand way, also a constant and cannot be changed anytime in program
+	apple := "apple"
+	fmt.Println(apple)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	//will print 0 because int is intialised with 0
+	fmt.Println(e)
+
+}
